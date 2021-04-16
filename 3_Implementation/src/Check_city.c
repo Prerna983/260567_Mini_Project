e712faf195869ae7803db3b4f4db6ce223013161
Prerna983 <80800016+Prerna983@users.noreply.github.com>
2021-04-16 12:26:59 +0530

    finally updated

--- 3_Implementation/src/Check_city.c
+++ 3_Implementation/src/Check_city.c
@@ -1,14 +1,18 @@
 #include "header.h"
 
+/* a character array of strings that stores the name of all the states */
 char* states[]={"andhra pradesh","arunachal pradesh","assam","bihar","chandigarh","chattisgarh","goa","gujarat","haryana","himachal pradesh","jammu and kashmir","jharkhand","karnataka","kerala","madhya pradesh","maharashtra","manipur","meghalaya","mizoram","nagaland","orissa","punjab","rajasthan","sikkim","tamil nadu","telangana","tripura","uttar pradesh","west bengal"};
+
+/* function to check if the state entered is coorect */
 int validity(char ch[])
 {
-    
+    /* finding total number of states in the character array (29 here) */
     int len=sizeof(states)/sizeof(states[0]);
     int value=0;
     int l=strlen(ch);
     char nstr[l];
     
+    /* loop to convert all the characters into lower case and adding it to the new string */
     for(int i=0;i<=l;i++)
     {
         
@@ -17,7 +21,8 @@ int validity(char ch[])
         else
             nstr[i]=' ';
     }
-
+    
+    /* loop to check that the new string entered is equal with the states mentioned or not */
     for(int i=0;i<len;i++)
     {
         if(strcmp(nstr,states[i])==0)
